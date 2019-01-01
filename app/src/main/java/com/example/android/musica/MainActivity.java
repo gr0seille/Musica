b6b2b99dcc4be8d84b33e248604afe0afb1e0cdb
gr0seille <unautrecomptetest22@gmail.com>
2019-01-01 13:55:50 +0100

    Open Activity through onClickListener

--- app/src/main/java/com/example/android/musica/MainActivity.java
+++ app/src/main/java/com/example/android/musica/MainActivity.java
@@ -7,13 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
-    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,40 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
         SongAdapter adapter = new SongAdapter(MainActivity.this, songs);
         mListView.setAdapter(adapter);
+        TextView playerLauncher ;
+        playerLauncher=(TextView) findViewById(R.id.launcher);
+        playerLauncher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),playerActivity.class);
+                startActivity(i);
+            }
+        });
     }
-
-    public void openPlayerActivity(View view) {
-        Intent i = new Intent(this, playerActivity.class);
-        startActivity(i);
-    }
-
-//    //add listener on button
-//    public void addListenerOnButton() {
-//
-//        button = (Button) findViewById(R.id.launcher);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View arg0) {
-//
-//                Intent browserIntent =
-//                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
-//                startActivity(browserIntent);
-//
-//            }
-//
-//        });
-//    }
 
 
     private List<Song> buildSongs() {
         List<Song> songs = new ArrayList<Song>();
-        songs.add(new Song(1, 333, "Florent", "Mon premier tweet !", R.drawable.album1));
-        songs.add(new Song(2,333, "Kevin", "C'est ici que ça se passe !", R.drawable.album2));
-        songs.add(new Song(3,333, "Logan", "Que c'est beau...", R.drawable.album3));
-        songs.add(new Song(4,333, "Mathieu", "Il est quelle heure ??", R.drawable.album4));
-        songs.add(new Song(5,333, "Willy", "On y est presque", R.drawable.album1));
+        songs.add(new Song(1, 333, "Hit sale", "Thérapie taxi", R.drawable.album1));
+        songs.add(new Song(2, 333, "Aerials", "C'est ici que ça se passe !", R.drawable.album2));
+        songs.add(new Song(3, 333, "Feel it still", "Que c'est beau...", R.drawable.album3));
+        songs.add(new Song(4, 333, "Un autre que moi", "Fishbach", R.drawable.album4));
+        songs.add(new Song(5, 333, "Breezblocks", "Alt-J", R.drawable.album1));
         return songs;
     }
 
